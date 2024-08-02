7747be794381cb3fe957c4e3332308de4bbf31f0
Soumik Sinha <soumiksingha8@gmailcom>
2024-08-02 13:58:21 +0530

    add recursion

---
+++ Recursion/Recursion1.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class Recursion1 {
+
+    public static void printNum(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        printNum(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+
+        printNum(num);
+    }
+}
