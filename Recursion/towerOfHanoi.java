8f383d0b8dd81ff03b16d7b42eb067a7d6c3c7e3
Soumik Sinha <soumiksingha8@gmailcom>
2024-08-03 10:53:59 +0530

    add remaining

---
+++ Recursion/towerOfHanoi.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class towerOfHanoi {
+
+    public static void diskShift(int n, String src, String helper, String dest) {
+        if (n == 1) {
+            System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
+            return;
+        }
+
+        diskShift(n - 1, src, dest, helper);
+        System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
+        diskShift(n - 1, helper, src, dest);
+    }
+
+    public static void main(String[] args) {
+        int n = 2;
+        diskShift(n, "S", "H", "D");
+    }
+}
