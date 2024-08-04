b37a279b763d54f53b8779b11202fdd0d7f99344
Soumik Sinha <soumiksingha8@gmailcom>
2024-08-04 18:19:04 +0530

    add

---
+++ Recursion/removeDuplicates.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+public class removeDuplicates {
+    public static boolean[] map = new boolean[26];
+
+    public static void revDup(String strn, int index, String newString) {
+        if (index == strn.length()) {
+            System.out.println(newString);
+            System.out.println(index);
+            return;
+        }
+        char character = strn.charAt(index);
+        if (map[character - 'a']) {
+            revDup(strn, index + 1, newString);
+        } else {
+            newString += character;
+            map[character - 'a'] = true;
+            revDup(strn, index + 1, newString);
+        }
+    }
+
+    public static void main(String[] args) {
+        String strn = "aabbbccdd";
+        revDup(strn, 0, "");
+    }
+}
